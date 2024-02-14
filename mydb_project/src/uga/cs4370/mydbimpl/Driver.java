9b751aea50ff701067d61b551f81f446698991bd
Daniel Mora Galazzo <daalmoga@gmail.com>
2024-02-14 16:12:54 -0500

    Update Driver.java

--- mydb_project/src/uga/cs4370/mydbimpl/Driver.java
+++ mydb_project/src/uga/cs4370/mydbimpl/Driver.java
@@ -19,15 +19,15 @@ public class Driver {
                 .attributeNames(List.of("id", "name", "dept", "salary"))
                 .attributeTypes(List.of(Type.INTEGER, Type.STRING, Type.STRING, Type.DOUBLE))
                 .build();
-        rel1.loadData("/Users/harrisonkirstein/Desktop/CSCI4370-Project1/instructor_export.csv");
-        //rel1.loadData("C:\\Users\\daalm\\Documents\\GitHub\\CSCI4370-Project1\\mydb_project\\src\\uga\\cs4370\\mydbimpl\\mysql-files\\instructor_export.csv");
+        //rel1.loadData("/Users/harrisonkirstein/Desktop/CSCI4370-Project1/instructor_export.csv");
+        rel1.loadData("C:\\Users\\daalm\\Documents\\GitHub\\CSCI4370-Project1\\mydb_project\\src\\uga\\cs4370\\mydbimpl\\mysql-files\\instructor_export.csv");
 
         Relation rel2 = new RelationBuilder()
         .attributeNames(List.of("id", "name", "dept", "tot_cred"))
         .attributeTypes(List.of(Type.INTEGER, Type.STRING, Type.STRING, Type.DOUBLE))
         .build();
-        rel2.loadData("/Users/harrisonkirstein/Desktop/CSCI4370-Project1/student_export.csv");
-        //rel2.loadData("C:\\Users\\daalm\\Documents\\GitHub\\CSCI4370-Project1\\mydb_project\\src\\uga\\cs4370\\mydbimpl\\mysql-files\\student_export.csv");
+        //rel2.loadData("/Users/harrisonkirstein/Desktop/CSCI4370-Project1/student_export.csv");
+        rel2.loadData("C:\\Users\\daalm\\Documents\\GitHub\\CSCI4370-Project1\\mydb_project\\src\\uga\\cs4370\\mydbimpl\\mysql-files\\student_export.csv");
 
         List<String> testList = new ArrayList<>();
         testList.add("name");
@@ -35,9 +35,11 @@ public class Driver {
         testList.add("dept");
         RA ra = new Implementation();
 
-       //ra.project(rel1, testList).print();
-
-        //ra.select(rel1,(row)-> row.get(rel1.getAttrIndex("instr_id").equals(cell.val((34175))))).print();
+        //Queries go here, must have more than 1 table used also between 1 and 50 rows
+        
+        
+        //ra.project(rel1, testList).print();
+        //ra.cartesianProduct(rel2, ra.project(rel1, testList)).print();
         //ra.cartesianProduct(rel1, rel2).print();
 
         // Relation rel2 = new RelationBuilder()
@@ -47,7 +49,7 @@ public class Driver {
         // rel2.loadData("/Users/harrisonkirstein/Desktop/mydb_project/activity_2/mysql-files/student_export.csv");
         // rel2.print();
  
-        // rel instr10001 = ra.select(instructor,(row)-> row.get(instr.getAttrIndex("instr_id").equals(cell.val(10001))))
+        // rel instr10001 = ra.select(instructor,(row)-> row.get(instr.getAttrIndex("instr_id").equals(cell.val(34175))))
         // instr10000.print();
     }
 }
